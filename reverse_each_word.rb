7489461f4c95ca059541388495614a5a339860c6
JC Chang <jerryjcchang@gmail.com>
2018-11-09 21:15:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
-  array.collect do|string|
-    test_array << string.reverse #reverse each word in the array
-  end
+  array.collect do |string|
+    test_array << string.reverse
   test_array.join(" ")
 end
